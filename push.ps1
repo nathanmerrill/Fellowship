21c8bbdb28a18a66d04bd23a8dd09ed0531d9e72
Nathan Merrill <nathan.t.merrill@gmail.com>
2016-11-14 05:58:47 -0700

    Small fix to push script

--- push.ps1
+++ push.ps1
@@ -10,14 +10,12 @@ git checkout master
 }
 if (!([string]::IsNullOrEmpty($FellowshipMessage))){
 gradle build
-java -jar build/libs/Fellowship.jar download compile
 git add .
 git commit -m "$FellowshipMessage"
 git push origin master
 git checkout archives
 git merge master
-cp build/libs/Fellowship.jar Fellowship.jar
-7z a Fellowship.zip Fellowship.jar -aou
+7z a Fellowship.7z build/libs/Fellowship.jar -aou
 git add .
 git commit -m "$FellowshipMessage"
 git push origin archives
