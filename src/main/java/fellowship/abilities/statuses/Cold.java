97360b5193eacc7ec9e8bf9b8240cf463e9cdc89
Nathan Merrill <nathan.t.merrill@gmail.com>
2016-11-15 10:20:08 -0700

    Fix for Cold

--- src/main/java/fellowship/abilities/statuses/Cold.java
+++ src/main/java/fellowship/abilities/statuses/Cold.java
@@ -9,10 +9,10 @@ public class Cold extends Ability {
 
     @Override
     public void apply(BaseCharacter character) {
-        character.visibleEnemies(1).forEach(enemy -> {
+        character.on(Events.TurnStart, Event.forever(f -> character.visibleEnemies(1).forEach(enemy -> {
             enemy.slow(10);
             character.on(Events.TurnStart, Event.after(1, e -> enemy.slow(-10)));
-        });
+        })));
     }
 
 }
