afdf8018670df1c06b6da908f60b161d59ea77df
Nathan Merrill <nathan.t.merrill@gmail.com>
2016-11-18 10:06:54 -0700

    Fix to reflexive

--- src/main/java/fellowship/abilities/attacking/Reflexive.java
+++ src/main/java/fellowship/abilities/attacking/Reflexive.java
@@ -8,8 +8,12 @@ import fellowship.events.SliceEvent;
 public class Reflexive extends Ability {
     @Override
     public void apply(BaseCharacter character) {
-        Ability.addCooldown(3, character, Events.Slice, event -> {
-            character.slice(((SliceEvent)event).getSlicer());
+        Ability.addCooldown(3, character, Events.Sliced, event -> {
+            BaseCharacter slicer = ((SliceEvent)event).getSlicer();
+            if (slicer.getAbilities().anySatisfy(i -> i.toString().equals("Reflexive"))){
+                return;
+            }
+            character.slice(slicer);
         });
     }
 }
